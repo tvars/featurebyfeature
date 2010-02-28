5a97bdb32edf5bd9eb58f38c91396d4da9af5bea
Tomas Varsavsky <tvarsavsky@thoughtworks.com>
2010-02-28 21:25:38 +1100

    added validation for feature requests

--- app/controllers/feature_requests_controller.rb
+++ app/controllers/feature_requests_controller.rb
@@ -3,14 +3,19 @@ class FeatureRequestsController < ApplicationController
   before_filter :login_required, :only => [:done]
 
   def index
-    @backlog_feature_requests = FeatureRequest.all(:conditions => ['status = ?', FeatureRequest::NEW_STATUS], :order => 'votes desc')
-    @done_feature_requests = FeatureRequest.all(:conditions => ['status = ?', FeatureRequest::DONE_STATUS], :order => 'updated_at desc')
     @new_feature_request = FeatureRequest.new
+    load_feature_requests
   end
 
   def create
-    FeatureRequest.create!(params['feature_request'])
-    redirect_to :action => :index
+    @new_feature_request = FeatureRequest.new(params['feature_request'])
+    if @new_feature_request.valid?
+      @new_feature_request.save!
+      redirect_to :action => :index
+    else
+      load_feature_requests
+      render :action => 'index'
+    end
   end
 
   def vote
@@ -23,6 +28,13 @@ class FeatureRequestsController < ApplicationController
     feature_request = FeatureRequest.find(params[:id])
     feature_request.done
     redirect_to :action => :index
+  end
+
+  private
+
+  def load_feature_requests
+    @backlog_feature_requests = FeatureRequest.all(:conditions => ['status = ?', FeatureRequest::NEW_STATUS], :order => 'votes desc')
+    @done_feature_requests = FeatureRequest.all(:conditions => ['status = ?', FeatureRequest::DONE_STATUS], :order => 'updated_at desc')
   end
   
 end
--- app/models/feature_request.rb
+++ app/models/feature_request.rb
@@ -3,6 +3,9 @@ class FeatureRequest < ActiveRecord::Base
   NEW_STATUS = 'new'
   DONE_STATUS = 'done'
 
+  validates_presence_of :description, :submitted_by_name, :submitted_by_email
+  validates_format_of :submitted_by_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
   def vote
     self.update_attribute(:votes, self.votes + 1)
   end
